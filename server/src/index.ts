0327d899e7179083438eb12841e3a231fea64bc7
Julia Linek <leeenek0@gmail.com>
2022-07-10 16:22:28 +0200

    Connect with database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,27 @@
-import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
+require('dotenv').config();
 
+const express = require('express');
+
+const app = express();
+
+// limit: Controls the maximum request body size
+// extended: 	This option allows to choose between parsing the URL-encoded data with the querystring library (when false) or the qs library (when true).
+
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const uri = process.env.CONNECTION_URL as string;
+const port = process.env.PORT;
+
+// useNewUrlParser, useUnifiedTopology, useFindAndModify, and useCreateIndex are no longer supported options
+// https://mongoosejs.com/docs/migrating_to_6.html#no-more-deprecation-warning-options
+
+mongoose
+  .connect(uri)
+  .then(() =>
+    app.listen(port, () => console.log(`Server running on port: ${port}`))
+  )
+  .catch((error) => console.log(error.message));
